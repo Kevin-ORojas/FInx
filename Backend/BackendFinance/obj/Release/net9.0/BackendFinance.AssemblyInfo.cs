4f5beb788460e5d7eef816b10906ad41d8f5ea56
Kevin-ORojas <kevinroj326@gmail.com>
2025-05-31 14:41:36 -0500

    probando ramas

--- Backend/BackendFinance/obj/Release/net9.0/BackendFinance.AssemblyInfo.cs
+++ Backend/BackendFinance/obj/Release/net9.0/BackendFinance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendFinance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6786def47f0a1d007071d4b77ab0fe7513a323af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9bba7ab8cbf099096f18f1e653adfe68e078be")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendFinance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendFinance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
