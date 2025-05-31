f789eb3d01e4f3d1b32dc69cccf882f4ae11104e
Kevin-ORojas <kevinroj326@gmail.com>
2025-05-31 15:41:22 -0500

    ajustando

--- Backend/BackendFinance/Models/User.cs
+++ Backend/BackendFinance/Models/User.cs
@@ -1,5 +1,8 @@
 public class User
 {
-     public int Id { get; set; }
-    public string Name { get; set; }
+    public int Id { get; set; } // identificador unico en la tabla
+    public string Name { get; set; } // Nombre del usuario
+    public string Email { get; set; } // Nombre del email
+    public string PasswordHash { get; set; } // Contraseña encriptada
+    public DateTime CreateAt { get; set; } // creata fecha actual
 }
