57da6742fa66e162ae04717d4ca6e9b881753e94
Kevin-ORojas <kevinroj326@gmail.com>
2025-05-31 16:30:09 -0500

    intentando migrar para hacer la conexion a la db

--- Backend/BackendFinance/Data/AplicationDbContext.cs
+++ Backend/BackendFinance/Data/AplicationDbContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 
+// crea un puente entre tu aplicacion y la bade de datos usando entity framework
 public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         { }
+
     public DbSet<User> Users { get; set; }
 }
+
+
