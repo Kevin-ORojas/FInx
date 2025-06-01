0f46adde501617e73ec28a2f7a114f5042235d3c
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-01 10:01:45 -0500

    agregando el modelo de loans prestamos

--- Backend/Backend/models/Loans.cs
+++ Backend/Backend/models/Loans.cs
@@ -1,1 +1,16 @@
-sname
+namespace Backend.Models;
+
+public class Loans
+{
+    public int Id { get; set; }
+    public required int UserId { get; set; }
+    public decimal Amount { get; set; }
+    public string Leader { get; set; } = string.Empty;
+
+    public DateTime LoanDate { get; set; } = DateTime.UtcNow;
+    public DateTime DueDate { get; set; } = DateTime.UtcNow.AddDays(30);
+    public bool isPaid { get; set; } = false;
+
+    public User User { get; set; }
+
+}
