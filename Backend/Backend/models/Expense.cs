fc6737b9bc0aede2f4f108907111a0b228dc0231
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-01 09:54:37 -0500

    creando los modelos de gastos

---
+++ Backend/Backend/models/Expense.cs
@@ -0,0 +1,13 @@
+namespace Backend.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public required string Description { get; set; } = string.Empty;
+    public required decimal Amount { get; set; }
+    public required string Category { get; set; } = string.Empty;
+    public string PaymentMethod { get; set; } = string.Empty;
+    public required DateTime Date { get; set; } = DateTime.UtcNow;
+
+    public User user { get; set; }
+}
