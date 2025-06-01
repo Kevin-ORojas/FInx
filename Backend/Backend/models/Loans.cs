5915fbc059e01b835ba448b4a86a99854c66c80d
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-01 13:12:44 -0500

    nuevamente conflicto con la rama

--- Backend/Backend/models/Loans.cs
+++ Backend/Backend/models/Loans.cs
@@ -6,7 +6,6 @@ public class Loans
     public required int UserId { get; set; }
     public decimal Amount { get; set; }
     public string Leader { get; set; } = string.Empty;
-
     public DateTime LoanDate { get; set; } = DateTime.UtcNow;
     public DateTime DueDate { get; set; } = DateTime.UtcNow.AddDays(30);
     public bool isPaid { get; set; } = false;
---
+++ Backend/Backend/models/RecurringPayments.cs
@@ -0,0 +1,11 @@
+namespace Backend.Models;
+
+public class RecurringPayments
+{
+    public int Id { get; set; }
+    public required int UserId { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateTime NextPaymentDate { get; set; } = DateTime.UtcNow;
+    public User User { get; set; }
+}
