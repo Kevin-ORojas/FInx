c59e5e5b37373f3744da8d3aa8f72b3f20761715
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-02 09:23:31 -0500

    verificamos si la contraseña esta hasheada

--- Backend/Backend/services/UserService.cs
+++ Backend/Backend/services/UserService.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 
 
-namespace Backend.Services {
-    public class UserService {
-        private readonly List<User> _users = new(){
-            new User {Id = 1, Name = "Alice", Email ="alice@gmail.com", PasswordHash = "hashed_password", CreatedAt = DateTime.UtcNow},
-             new User {Id = 1, Name = "Alexa", Email ="Alexa@gmail.com", PasswordHash = "hashed_password", CreatedAt = DateTime.UtcNow}
-        };
+namespace Backend.Services 
+{
+    public class UserService
+     {
+        private readonly List<User> _users = new();
+        
+        public User CreateUser(User newUser)
 
-            public IEnumerable<User> GetUsers() => _users;
+        {
+            newUser.Id = _users.Count + 1;
+            newUser.CreatedAt = DateTime.UtcNow;
 
-            public User? GetUserById(int id) => _users.FirstOrDefault(u => u.Id == id);
+            // see Hash the password before the save!
+            newUser.PasswordHash = BCrypt.Net.Bcrypt.HashPassword(newUser.PasswordHash);
 
-            public User CreateUser (User newUser) {
-                newUser.Id = _users.Max(u => u.Id) +1;
-                newUser.CreatedAt = DateTime.UtcNow;
-                _users.Add(newUser);
-                return newUser;
-            }
+            _users.Add(newUser);
+            return newUser;
+        }
+   
+       pubblic User? GetUserByEmail(string email)
+       {
+        return _users.FirstOrDefault(u => u.Email == email);
+       }
+
+       public bool VerifyPassword(string inputPassword, string storedHash)
+       {
+        return BCrypt.Net.BCrypt.Verify(inputPassword, storedHash);
+       }
         
     }
 }
