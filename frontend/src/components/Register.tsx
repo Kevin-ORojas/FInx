5c453c15d5f2f70adfa60e8cfaa4cbf0d6bfa36d
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-06 18:18:53 -0500

    haciendo formulario

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,23 +1,56 @@
+import { useState } from "react";
+import type { Usuario } from "../interfaces/usuario";
+
 export default function Register() {
+  const [formDate, setFormDate] = useState<Usuario>({
+    nombre: "",
+    email: "",
+    password: "",
+  });
 
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
+  // const { nombre, email, password } = formDate;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormDate({
+      ...formDate,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+
+    setTimeout(() => {
+      console.log("Formulario enviado", formDate);
+      setIsSubmitted(false);
+    }, 2000);
+  };
   return (
     <div className="flex justify-center items-center h-screen">
-      <form action="">
+      <form onSubmit={onSubmit}>
         <div>
           <label htmlFor="">Pruebas</label>
-          <input type="text" name="" id="" />
+          <input type="text" name="name" id="name" onChange={onChange} />
         </div>
         <div>
           <label htmlFor="">Pruebas</label>
-          <input type="text" />
+          <input type="email" name="email" id="email" onChange={onChange} />
         </div>
         <div>
           <label htmlFor="">Pruebas</label>
-          <input type="text" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onSubmit={onChange}
+          />
         </div>
 
-        <button>{"Registrate" && "Login"}</button>
+        <button type="submit" disabled={isSubmitted}>
+          {isSubmitted ? "Register" : "Login"}{" "}
+        </button>
       </form>
     </div>
   );
