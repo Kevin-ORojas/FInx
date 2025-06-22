879e98a8df589c3ae9389dea90e830c7b9f6ef3d
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-22 14:44:00 -0500

    colcando cajas para hacer modelos de lo que va dentro de cada caja

--- frontend/src/components/Loans.tsx
+++ frontend/src/components/Loans.tsx
@@ -1,9 +1,86 @@
-
-
 export default function Loans() {
   return (
-    <div>
-      Loans
+    <div className="grid grid-cols-1 w-full mt-4 justify-items-start p-2 gap-4">
+      <div className="border border-red-200 w-full h-auto mt-10">
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            molestiae dolore impedit atque recusandae ad voluptas dolorum, qui
+            eveniet aperiam minima reprehenderit quasi non officiis accusamus,
+            esse nam dolores debitis! Alias reiciendis odit ab quos? At
+            praesentium dolorum numquam culpa qui. Nisi atque, illo provident
+            minus ipsam ex hic sed fugiat reprehenderit, laborum quibusdam et,
+            culpa sapiente vitae blanditiis quos! Repellendus officiis ab
+            tempora! Sed, magnam! Vel, consequatur! Magnam optio aspernatur quia
+            eum error voluptatibus quo earum dolorum, provident nihil. Ea quo
+            nihil doloribus dolorem aperiam veniam omnis a perspiciatis. Dicta
+            fugit autem est repellendus rerum sit, possimus cumque corporis rem
+            ipsam error quis velit voluptatum optio in laborum impedit qui
+            sapiente, illo magni molestias! Eos eligendi sint aperiam alias.
+            Pariatur quasi fuga corrupti, modi vel sapiente consectetur alias
+            libero eum illo reiciendis, aliquam velit. Amet natus autem cumque,
+            quia repudiandae nostrum, optio iste incidunt expedita
+            necessitatibus delectus, aut neque? Dignissimos porro nesciunt unde
+            cum officiis reprehenderit! Vitae nihil ducimus, molestiae animi
+            fugit corporis assumenda porro possimus dolor atque! Veniam optio
+            autem tempore ipsum itaque officiis alias, ea voluptatem molestias?
+          </p>
+        </div>
+      </div>
+
+      <div className="border border-red-200 w-full h-auto mt-10">
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            molestiae dolore impedit atque recusandae ad voluptas dolorum, qui
+            eveniet aperiam minima reprehenderit quasi non officiis accusamus,
+            esse nam dolores debitis! Alias reiciendis odit ab quos? At
+            praesentium dolorum numquam culpa qui. Nisi atque, illo provident
+            minus ipsam ex hic sed fugiat reprehenderit, laborum quibusdam et,
+            culpa sapiente vitae blanditiis quos! Repellendus officiis ab
+            tempora! Sed, magnam! Vel, consequatur! Magnam optio aspernatur quia
+            eum error voluptatibus quo earum dolorum, provident nihil. Ea quo
+            nihil doloribus dolorem aperiam veniam omnis a perspiciatis. Dicta
+            fugit autem est repellendus rerum sit, possimus cumque corporis rem
+            ipsam error quis velit voluptatum optio in laborum impedit qui
+            sapiente, illo magni molestias! Eos eligendi sint aperiam alias.
+            Pariatur quasi fuga corrupti, modi vel sapiente consectetur alias
+            libero eum illo reiciendis, aliquam velit. Amet natus autem cumque,
+            quia repudiandae nostrum, optio iste incidunt expedita
+            necessitatibus delectus, aut neque? Dignissimos porro nesciunt unde
+            cum officiis reprehenderit! Vitae nihil ducimus, molestiae animi
+            fugit corporis assumenda porro possimus dolor atque! Veniam optio
+            autem tempore ipsum itaque officiis alias, ea voluptatem molestias?
+          </p>
+        </div>
+      </div>
+
+      <div className="border border-red-200 w-full h-auto mt-10">
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            molestiae dolore impedit atque recusandae ad voluptas dolorum, qui
+            eveniet aperiam minima reprehenderit quasi non officiis accusamus,
+            esse nam dolores debitis! Alias reiciendis odit ab quos? At
+            praesentium dolorum numquam culpa qui. Nisi atque, illo provident
+            minus ipsam ex hic sed fugiat reprehenderit, laborum quibusdam et,
+            culpa sapiente vitae blanditiis quos! Repellendus officiis ab
+            tempora! Sed, magnam! Vel, consequatur! Magnam optio aspernatur quia
+            eum error voluptatibus quo earum dolorum, provident nihil. Ea quo
+            nihil doloribus dolorem aperiam veniam omnis a perspiciatis. Dicta
+            fugit autem est repellendus rerum sit, possimus cumque corporis rem
+            ipsam error quis velit voluptatum optio in laborum impedit qui
+            sapiente, illo magni molestias! Eos eligendi sint aperiam alias.
+            Pariatur quasi fuga corrupti, modi vel sapiente consectetur alias
+            libero eum illo reiciendis, aliquam velit. Amet natus autem cumque,
+            quia repudiandae nostrum, optio iste incidunt expedita
+            necessitatibus delectus, aut neque? Dignissimos porro nesciunt unde
+            cum officiis reprehenderit! Vitae nihil ducimus, molestiae animi
+            fugit corporis assumenda porro possimus dolor atque! Veniam optio
+            autem tempore ipsum itaque officiis alias, ea voluptatem molestias?
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
