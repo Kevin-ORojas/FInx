204f4501596e122a97d2650bb82ca0d750627f3c
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-22 14:52:33 -0500

    haciendo tarjetas del home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,10 +1,25 @@
 export default function Home() {
   return (
-    <div className="text-black p-2 ">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem libero
-      illo dolore ratione pariatur amet distinctio obcaecati nihil esse minus
-      asperiores impedit, repellendus suscipit accusamus, qui fugiat. Adipisci
-      et deserunt accusantium quaerat tempora molestiae doloremque?
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 ">
+      <div className="bg-white shadow-md p-6 rounded-lg hover:scale-90 transition-transform cursor-pointer">
+        <h2 className="text-gray font-[lato] font-bold mb-2">Saldo Actual:</h2>
+        <p className="text-2xl text-green-500 font-semibold">$3,200</p>
+      </div>
+
+      <div className="bg-white shadow-md p-6 rounded-lg hover:scale-90 transition-transform cursor-pointer">
+        <h2 className="text-gray font-[lato] font-bold mb-2">Saldo Actual:</h2>
+        <p className="text-2xl text-red-500 font-semibold">$3,200</p>
+      </div>
+
+      <div className="bg-white shadow-md p-6 rounded-lg hover:scale-90 transition-transform cursor-pointer">
+        <h2 className="text-gray font-[lato] font-bold mb-2">Saldo Actual:</h2>
+        <p className="text-2xl text-blue-500 font-semibold">$3,200</p>
+      </div>
+
+      <div className="bg-white shadow-md p-6 rounded-lg hover:scale-90 transition-transform cursor-pointer">
+        <h2 className="text-gray font-[lato] font-bold mb-2">Saldo Actual:</h2>
+        <p className="text-2xl text-yellow-500 font-semibold">$3,200</p>
+      </div>
     </div>
   );
 }
