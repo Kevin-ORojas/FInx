60357f99cb013511e3923001ef1a644c4687c622
Kevin-ORojas <kevinroj326@gmail.com>
2025-06-28 22:11:51 -0500

    comentarios

--- Backend/Backend/models/User.cs
+++ Backend/Backend/models/User.cs
@@ -5,7 +5,7 @@ public class User
     public int Id { get; set; }
     public required string Name { get; set; } = string.Empty;
     public required string Email { get; set; } = string.Empty;
-    public required string PasswordHash { get; set; } = string.Empty;
+    public required string PasswordHash { get; set; } = string.Empty; //
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
    
